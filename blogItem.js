75497b5d116ed576cbdfc975028c453c4fdc111a
nazip <nazipm@mail.ru>
2016-12-22 09:52:49 +0300

    errors fixed

--- blogItem.js
+++ blogItem.js
@@ -1,6 +1,6 @@
 const DOM = React.DOM;
 
-const settings = {
+const imageSettings = {
   src : "https://cdn.scratch.mit.edu/static/site/users/avatars/1000/2402.png",
   width : 100,
   height : 100,
@@ -9,8 +9,8 @@ const settings = {
 
 class Image extends React.Component {
   render() {
-    const settings = this.props;
-    return <img alt={settings.alt} src={settings.src} width={settings.width} height={settings.height}/>;
+    const imageSettings = this.props;
+    return <img alt={imageSettings.alt} src={imageSettings.src} width={imageSettings.width} height={imageSettings.height}/>;
   }
 }
 
@@ -23,16 +23,16 @@ class TextBox extends React.Component {
 
 class BlogItem extends React.Component {
   render() {
-    const settings = this.props.settings;
+    const imageSettings = this.props.imageSettings;
     const text = this.props.children;
     
-    const image = React.createElement(Image, settings);
+    const image = React.createElement(Image, imageSettings);
     const textbox = React.createElement(TextBox, null, text);
     return <div>{image}{text}</div>;
   }
 }
 
 ReactDOM.render(
-    React.createElement(BlogItem, {settings}, "hello"),
+    React.createElement(BlogItem, {imageSettings}, "hello"),
     document.getElementById("app")
 );
--- bloglist.js
+++ bloglist.js
@@ -1,81 +1,95 @@
 const DOM = React.DOM;
 
-const settings = {
-  src : "https://cdn.scratch.mit.edu/static/site/users/avatars/1000/2402.png",
-  width : 100,
-  height : 100,
-  alt : "BMO"
+const imageSettings = {
+    src: "https://cdn.scratch.mit.edu/static/site/users/avatars/1000/2402.png",
+    width: 100,
+    height: 100,
+    alt: "BMO"
 }
 
 const items = [
-  {
-    src : "https://cdn.scratch.mit.edu/static/site/users/avatars/1000/2402.png",
-    text : "first",
-    width : 100,
-    height: 100
+    {
+        image: {
+            src: "https://cdn.scratch.mit.edu/static/site/users/avatars/1000/2402.png",
+            width: 100,
+            height: 100
+        },
+        text: "first"
   },
-  
-  {
-    src : "http://vignette2.wikia.nocookie.net/adventuretimewithfinnandjake/images/8/83/1AT_ice_king_character.png/revision/latest?cb=20110915231820",
-    text : "second",
-    width : 100,
-    height: 150
+
+    {
+        image: {
+            src: "https://cdn.scratch.mit.edu/static/site/users/avatars/1000/2402.png",
+            width: 100,
+            height: 100
+        },
+        text: "second"
   },
-  
-  {
-    src : "http://vignette2.wikia.nocookie.net/adventuretimewithfinnandjake/images/d/d8/Candy_Person_111.png/revision/latest?cb=20120810072803",
-    text : "third",
-    width : 100,
-    height: 100
+
+    {
+        image: {
+            src: "https://cdn.scratch.mit.edu/static/site/users/avatars/1000/2402.png",
+            width: 100,
+            height: 100
+        },
+        text: "third"
+  },
+    {
+        image: {
+            src: "https://cdn.scratch.mit.edu/static/site/users/avatars/1000/2402.png",
+            width: 100,
+            height: 100
+        },
+        text: "fourth"
   }
+
 ];
 
 class Image extends React.Component {
-  render() {
-    const {settings} = this.props;
-    return <img src={settings.src} width={settings.width} height={settings.height} alt={settings.alt}/>;
-  }
+    render() {
+        const {imageSettings} = this.props;
+        return <img src = {imageSettings.src}
+        width = {imageSettings.width}
+        height = {imageSettings.height}
+        alt = {imageSettings.alt}/>;
+    }
 }
 
 class TextBox extends React.Component {
-  render() {
-    const text = this.props.children;
-    return <span>{text}</span>;
-  }
+    render() {
+        return <span > {this.props.children} < /span>;
+    }
 }
 
 class BlogItem extends React.Component {
-  render() {
-    console.log("BlogItem",this.props);
-    const {settings} = this.props;
-    const text = this.props.children;
-    
-    const image = React.createElement(Image, {settings});
-    const textbox = React.createElement(TextBox, null, text);
-    return React.createElement('div', null, image, textbox);
-  }
+    render() {
+        const {imageSettings} = this.props;
+        const text = this.props.children;
+
+        const image = React.createElement(Image, {imageSettings});
+        const textbox = React.createElement(TextBox, null, text);
+        return React.createElement('div', null, image, textbox);
+    }
 }
 
 class BlogList extends React.Component {
-  render() {
-    const {items} = this.props;
-    console.log(items);
-   return DOM.ul(
-    null,
-    _.map(
-      items,
-      (item, key) => {
-        const settings = item;
-        return DOM.li({ key }, React.createElement(BlogItem, {settings}, item.text));
-      }
-    )
-  );
-  }
+    render() {
+        const { items } = this.props;
+        return DOM.ul(
+            null,
+            _.map(
+                items, (item, key) => {
+                    const imageSettings = item.image;
+                    return DOM.li({ key }, React.createElement(BlogItem, {imageSettings}, item.text));
+                }
+            )
+        );
+    }
 }
 
 ReactDOM.render(
-    React.createElement(BlogList, {items}),
+    React.createElement(BlogList, {
+        items
+    }),
     document.getElementById("app")
 );
-
-
